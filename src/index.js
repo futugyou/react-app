3e28be1aa0dab025c147f015dd22c0ad473851af
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2020-12-11 13:12:47 +0000

    add base state demo
    add base state demo

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@
 // reportWebVitals();
 
 
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const tt = [1, 2, 3, 4]
@@ -31,11 +31,15 @@ const obj = {
 const sum = (num1, num2) => num1 + num2
 
 const Hello = (p) => {
+  const name = p.name
+  const age = p.age
+  const bornYear = () => new Date().getFullYear() - age
+
   return (
     <div>
       <p>Hello {p.name} age {p.age} like {p.like}</p>
       <p>{p.obj} sum is {p.sum}</p>
-       
+      <p>so you were probdbly born in {bornYear()}</p>
     </div>
   )
 }
@@ -51,6 +55,14 @@ const App = () => {
   const now = new Date()
   const a = 10
   const b = 20
+  const [couter, setCouter] = useState(0)
+  const handClick = () => {
+    console.log('clicked')
+  }
+
+  // setTimeout(() => {
+  //   setCouter(couter + 1)
+  // }, 1000);
   // return (
   //   <div>
   //     <p>Hello world, it si {now.toString()}</p>
@@ -59,21 +71,38 @@ const App = () => {
   //     </p>
   //   </div>
   // )
-  return React.createElement(
-    'div',
-    null,
-    React.createElement(
-      'p', null, 'hello world , it is ', now.toString()
-    ),
-    React.createElement(
-      'p', null, a, ' puls ', b, ' is ', a + b
-    ),
-    <Hello name="vishel" age={b} like={tt} obj={obj.second} sum = {sum(1,2)} parts={parts} />
-  
+  const increase = () => setCouter(couter + 1)
+  const decrease = () => setCouter(couter - 1)
+  const setZero = () => setCouter(0)
+  return (
+    <div>
+      {/* <div>{couter}</div> */}
+      <Display couter={couter} />
+      <Button handClick={increase} text="plus"></Button>
+      <Button handClick={decrease} text="de"></Button>
+      <Button handClick={setZero} text="zero"></Button>
+    </div>
   )
 }
+
+const Display = (props) => {
+  return (
+    <div>
+      {props.couter}
+    </div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.handClick}>
+      {props.text}
+    </button>
+  )
+}
+
 ReactDOM.render(
-  // <App />,
-  React.createElement(App, null),
+  <App />,
+  //React.createElement(App, null),
   document.getElementById('root')
 )
